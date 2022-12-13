d59a0cdcbb89d3a202f9d13e960173a1295cf16d
Supergrammer <supergrammer@naver.com>
2022-12-13 09:08:28 +0900

    [Feat] App.vue 에 scrollbar 제거 css 추가, #5

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,30 @@ import AppHeader from "@/components/app/AppHeader.vue";
 </script>
 
 <template>
-  <v-app>
-    <app-header id="appHeader" />
-    <router-view id="routerView" mainComponent />
-  </v-app>
+    <v-app id="app">
+        <app-header id="appHeader" />
+        <router-view id="routerView" />
+    </v-app>
 </template>
 
 <style scoped>
+#app {
+    /* IE and Edge */
+    -ms-overflow-style: none;
+    /* Firefox */
+    scrollbar-width: none;
+}
+
+#app::-webkit-scrollbar {
+    /* Chrome, Safari, Opera */
+    display: none;
+}
+
 #appHeader {
-  z-index: 20;
+    z-index: 20;
 }
 
 #routerView {
-  z-index: 10;
+    z-index: 10;
 }
 </style>
