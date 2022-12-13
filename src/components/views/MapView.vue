da54033d845533aa261b5cc51a4d0debdf177d41
Supergrammer <supergrammer@naver.com>
2022-12-13 09:07:19 +0900

    [Chore] MapView 컴포넌트를 컨테이너로 변경, #2

--- src/components/views/MapView.vue
+++ src/components/views/MapView.vue
@@ -1,38 +1,11 @@
 <script setup>
-import { latLng } from "leaflet";
-import {
-    LMap,
-    LTileLayer,
-    LMarker,
-} from "@vue-leaflet/vue-leaflet";
-import { ref } from "vue";
-import "leaflet/dist/leaflet.css";
-
-// LMap
-const zoom = ref(17);
-const center = [37.400977, 127.1042391];
-
-// LTileLayer
-const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-
-let markerLatLng = [37.400977, 127.1042391];
-let mapOptions = {
-    zoomSnap: 0.5,
-}
-
-const centerUpdate = function (center) {
-    console.log(center)
-}
-
+import MapContainer from "@/components/map/MapContainer.vue";
 </script>
 
 <template>
-    <l-map :zoom="zoom" :center="center" :options="mapOptions" :zoomAnimation="true" :minZoom="7" :maxZoom="17"
-        @update:center="centerUpdate" @update:zoom="centerUpdate">
-        <l-tile-layer :url="url" :options="mapOptions"></l-tile-layer>
-        <l-marker :lat-lng="markerLatLng"></l-marker>
-    </l-map>
-
+    <v-container fluid fill-height class="pa-0 fill-height">
+        <map-container />
+    </v-container>
 </template>
 
 <style scoped>
