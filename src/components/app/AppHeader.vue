910243d077e80c3baabd70a0fa7deeda281358a2
Supergrammer <supergrammer@naver.com>
2022-12-13 09:06:43 +0900

    [Feat] AppHeader 로고 추가 및 Hamburger icon 추가, #3

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,10 +1,14 @@
 <script setup>
+const logo = new URL("@/assets/logo.png", import.meta.url).href
 </script>
 
 <template>
-    <v-card :height="80" :elevation="10" class="d-flex align-center">
-        <v-icon icon="fa-solid fa-building-user" :size="32" class="mx-5" />
-        <span v-text="'WorkingTitleMap'" class="text-h5 font-weight-bold" />
+    <v-card :height="64" :elevation="10" class="pa-2 d-flex align-center">
+        <v-btn flat size="large" rounded="lg">
+            <v-icon icon="fa-solid fa-bars" :size="32" />
+        </v-btn>
+        <v-img :src="logo" :max-width="72" max-height="100%" />
+        <span v-text="'JIB SEE'" class="text-h4 font-weight-black" />
     </v-card>
 </template>
 
