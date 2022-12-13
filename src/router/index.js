0efb3077a1d825752a3ee6e89e2ae4df6b2d5033
Supergrammer <supergrammer@naver.com>
2022-12-13 09:09:43 +0900

    [Feat] /map/ path 추가 및 / redirect 설정, #2

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,29 @@ import { createRouter, createWebHistory } from "vue-router";
 import MapView from "@/components/views/MapView.vue"
 
 const routes = [
-  {
-    path: "/",
-    name: "map",
-    component: MapView,
-    // component: () => import("@/components/views/MapView.vue"),
-  },
+    {
+        path: "/",
+        redirect: { name: "mapPage" }
+        // component: () => import("@/components/views/MapView.vue"),
+    },
+    {
+        path: "/map/",
+        name: "mapPage",
+        component: MapView,
+        props: (route) => {
+            let { lat, lng, zoom } = route.query;
+            return {
+                lat: parseFloat(lat),
+                lng: parseFloat(lng),
+                zoom: parseFloat(zoom)
+            };
+        },
+    }
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
 });
 
 export default router;
